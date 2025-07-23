82cba389c5f3657d0446f8a0d31c612d708f3c98
dotnicode <romero.nico18@gmail.com>
2025-07-23 10:58:32 -0300

    refactor: refactor JWT settings to use central config

--- src/modules/auth/infrastructure/jwt/jwt.module.ts
+++ src/modules/auth/infrastructure/jwt/jwt.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 
+import { JWT_CONFIG } from './jwt.config';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     JwtModule.register({
-      secret: process.env.JWT_SECRET || 'secret',
-      signOptions: { expiresIn: '1h' },
+      secret: JWT_CONFIG.secret,
+      signOptions: { expiresIn: JWT_CONFIG.expiresIn },
     }),
     PassportModule,
   ],
--- src/modules/auth/infrastructure/jwt/jwt.strategy.ts
+++ src/modules/auth/infrastructure/jwt/jwt.strategy.ts
@@ -4,6 +4,7 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 
+import { JWT_CONFIG } from './jwt.config';
 import { IJwtPayload } from './jwt.interface';
 
 @Injectable()
@@ -12,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET,
+      secretOrKey: JWT_CONFIG.secret,
     });
   }
 
