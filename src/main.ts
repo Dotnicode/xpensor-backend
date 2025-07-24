7ad7c876f74ba22e8462b8ef8835baefa485c003
Nicolas Romero Olmos <romero.nico18@gmail.com>
2025-07-24 00:28:58 -0300

    feat: Add port logging when starting application
    Print the port number when the application starts to help with debugging and monitoring

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,9 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, () => {
+    console.log(`App running on port ${process.env.PORT ?? 3000}`);
+  });
 }
 
 void bootstrap();
