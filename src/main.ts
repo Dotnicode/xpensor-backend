1bdc908acfffdd26edba9c60752f42554dd2d659
Nico Olmos <romero.nico18@gmail.com>
2025-10-07 03:32:16 +0000

    refactor(main): remove duplicate import and enhance CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
+import { ConsoleLogger, Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConsoleLogger, Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -10,6 +10,14 @@ async function bootstrap() {
   });
 
   const logger = new Logger('Main');
+
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    credentials: true, // <- imprescindible para cookies
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    // opcional: exposedHeaders: ['Set-Cookie'], (no suele hacer falta)
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
